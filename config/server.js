b6c500c089f5fbf241bcef13abbdc8724030baa6
Alexander Revtov <alexander.revtov@gmail.com>
2021-11-17 22:39:51 +0200

    initial commit. First schemas on Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3735ec386c8e28657cd29ddc78de434a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '982e354b-caea-4ac8-a2f0-e2917f879235'
+};
